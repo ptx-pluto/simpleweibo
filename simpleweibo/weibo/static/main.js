3ee5d346c8d60b3ac242874a493866d913b64d64
sheep9275 <sheep9275@gmail.com>
2013-08-01 19:06:35 +0800

    minor

--- simpleweibo/weibo/static/main.js
+++ simpleweibo/weibo/static/main.js
@@ -2,7 +2,14 @@
     $('document').ready(function(){
 	$('.grid-tile').wookmark({
 	    container: $('#grid-container'),
-	    offset: 6,
+	    offset: 10,
+	    autoResize: true,
+	    align: 'center',
 	});
     });
+
+    $(window).load(function(){
+	$('#grid-container').trigger('refreshWookmark');
+    });
+
 })(jQuery);

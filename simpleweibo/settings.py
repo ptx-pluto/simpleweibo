f18f4218444d1557f2ae9888d74bcc020d7efde2
__PTX__ <sheep9275@gmail.com>
2013-10-08 01:33:38 +0800

    static file imported, use django static file resolver

--- simpleweibo/settings.py
+++ simpleweibo/settings.py
@@ -29,8 +29,7 @@ TEMPLATE_DIRS = (
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    join(PROJECT_ROOT, 'utils/static'),
 )
 
 #=================================================================
